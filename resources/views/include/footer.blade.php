985e3dacfb51ab4ad8923d1af4415749596c53b9
Raja <raja@raja.com>
2022-11-11 23:19:19 +0530

    all done

--- resources/views/include/footer.blade.php
+++ resources/views/include/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="bg-primary  rounded shadow p-2 mt-5 mb-0 footer">
     <p class="mb-0 text-center text-white "> copyright©{{ date('Y')}}-<a class=" text-success fw-normal"
             href="#" target="_blank"> {{env('APP_NAME')}}
-            </a> Powered By <a class="text-info" href="https://infinitenetsolutions.com/">Infinite Net Solutions</a></p>
+            </a> Powered By <a class="text-info" href="https://togoteams.com/"> Togo Teams</a></p>
 
 </footer>
