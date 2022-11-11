e237d5364320cb907cfe45f35cbeb8065ed07c52
Raja <raja@raja.com>
2022-11-12 00:54:13 +0530

    all done

--- resources/views/siteInfo/update.blade.php
+++ resources/views/siteInfo/update.blade.php
@@ -5,12 +5,12 @@
                 <ol class="breadcrumb breadcrumb-dark breadcrumb-transparent">
                     <li class="breadcrumb-item"><a href="{{ route('dashboard') }}"> <i class="fas fa-home    "></i> </a></li>
                     <li class="breadcrumb-item"><a href="#">{{ env('APP_NAME') }}</a></li>
-                    <li class="breadcrumb-item active" aria-current="page">Site Info</li>
+                    <li class="breadcrumb-item active" aria-current="page">Site Settting</li>
                 </ol>
             </nav>
             <div class="d-flex justify-content-between w-100 flex-wrap">
                 <div class="mb-3 mb-lg-0 col-10">
-                    <h1 class="h4">Site Info</h1>
+                    <h1 class="h4">Site Setting</h1>
                 </div>
 
             </div>
