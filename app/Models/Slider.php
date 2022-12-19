893816d070f82c231c7fed91d0a0fb6929145b91
Amit Prajapati <78385573+amitconnect18@users.noreply.github.com>
2022-12-19 13:35:54 +0530

    Update Slider.php

--- app/Models/Slider.php
+++ app/Models/Slider.php
@@ -9,6 +9,7 @@ class Slider extends Model
 {
     use HasFactory;
     protected $guarded = [];
+    protected $table = 'slider_tbl';
     public function media()
     {
         return $this->morphOne('App\Models\Media', 'table')->where('deleted_at', null)->orderBy('id', 'DESC');
