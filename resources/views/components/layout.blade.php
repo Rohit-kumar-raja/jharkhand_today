389f425e331c1e8d2bede1f948f6964bfbc8e8fc
Amit Prajapati <78385573+amitconnect18@users.noreply.github.com>
2022-12-22 09:46:16 +0530

    Update layout.blade.php

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -27,7 +27,7 @@
     <!-- Scripts -->
     <script type="text/javascript" src="{{ url('assets/js/pramukhime.js')}}"></script>
 
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <!-- <script src="{{ asset('js/app.js') }}" defer></script> -->
 </head>
 
 <body>
